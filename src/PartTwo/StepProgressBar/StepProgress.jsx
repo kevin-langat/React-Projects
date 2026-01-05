5a437c9bc4e2b3b20784df3fba722b0491bfb2f4
Kevin Langat <kevinlangat419@gmail.com>
2026-01-05 21:56:39 +0300

    Did multi-step progress bar componennt which displays progress of all the steps available.

---
+++ src/PartTwo/StepProgressBar/StepProgress.jsx
@@ -0,0 +1,48 @@
+function StepProgress({ steps, activeStep, setActiveStep }) {
+  function handlePrevStep() {
+    setActiveStep((prevStep) => Math.max(prevStep - 1, 0));
+  }
+
+  function handleNextStep() {
+    setActiveStep((prevStep) => Math.min(prevStep + 1, steps.length - 1));
+  }
+
+  return (
+    <div className='flex flex-col items-center gap-5 w-full'>
+      <div className=' flex w-full flex-row'>
+        {steps && steps.length > 0
+          ? steps.map((step, index) => (
+              <div
+                className={` w-full rounded-full ${
+                  activeStep === index || activeStep > index
+                    ? 'bg-green-400'
+                    : 'bg-blue-500'
+                }  flex flex-col items-center justify-center`}
+                key={index}
+              >
+                {step}
+              </div>
+            ))
+          : null}
+      </div>
+      {/* 110 */}
+      <div className='flex flex-row gap-4'>
+        <button
+          disabled={activeStep === 0}
+          className='bg-blue-500 px-6 py-1 rounded-[0.2em] disabled:bg-gray-400'
+          onClick={handlePrevStep}
+        >
+          Previous
+        </button>
+        <button
+          className='bg-blue-500  px-6 py-1 rounded-[0.2em]                      disabled:bg-gray-400'
+          disabled={activeStep === steps.length - 1}
+          onClick={handleNextStep}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+export default StepProgress;
--- src/PartTwo/StepProgressBar/StepProgressBar.jsx
+++ src/PartTwo/StepProgressBar/StepProgressBar.jsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+import StepProgress from './StepProgress';
+
 function StepProgressBar() {
-  return <div>StepProgressBar</div>;
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4', 'Step 5'];
+  return (
+    <div className='w-3/4'>
+      <StepProgress
+        steps={steps}
+        setActiveStep={setActiveStep}
+        activeStep={activeStep}
+      />
+    </div>
+  );
 }
 export default StepProgressBar;
